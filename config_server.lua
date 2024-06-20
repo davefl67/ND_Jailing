d2d5a4a7dbc73ee87cdcf81d72cb0d03a41f62ca
Dave Lowe <107778859+davefl67@users.noreply.github.com>
2024-06-20 12:36:31 -0400

    Update config_server.lua

--- config_server.lua
+++ config_server.lua
@@ -1,3 +1,3 @@
 server_config = {
-	discordWebhook = "https://discord.com/api/webhooks/1153532965935136798/5pTVRjqBS12fwCa5bWnPkEWg9KH_Q0BmpvB6dyGb9cFLrNqiiiGMnQC5vMTRLT4OT4bK",
+	discordWebhook = "YOUR_WEBHOOK",
 }
